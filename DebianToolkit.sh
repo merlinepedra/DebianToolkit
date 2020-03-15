736595ef643698925bff0710e47aacded13f3aee
Miguel R <d4t4s3c@protonmail.com>
2020-03-15 07:54:02 +0100

    Update DebianToolkit.sh

--- DebianToolkit.sh
+++ DebianToolkit.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+#Author: d4t4s3c
+#Email:  d4t4s3c@protonmail.com
+#GitHub: www.github.com/d4t4s3c
+
+#colors
+b="\033[1;37m"
+r="\033[1;31m"
+v="\033[1;32m"
+a="\033[1;33m"
+nc="\e[0m"
+
+#var
